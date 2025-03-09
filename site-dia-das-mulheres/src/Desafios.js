13d7cdaf224556501298e9ab3f12d19e75548d07
André Oliveira <andrelitoscria@gmail.com>
2025-03-09 17:34:56 -0300

    .

--- site-dia-das-mulheres/src/Desafios.js
+++ site-dia-das-mulheres/src/Desafios.js
@@ -3,8 +3,30 @@ import React from 'react';
 function Desafios() {
   return (
     <div>
-      <h1>Desafios Atuais das Mulheres</h1>
+        <h1>Desafios Atuais das Mulheres</h1>
+    <div className="desafios-content">
+      <h3>Violência de gênero</h3>
+      <ul style={{ color: 'black' }}>
+          <li>Violência doméstica: A violência física, psicológica e sexual contra mulheres dentro de seus próprios lares é um problema grave e persistente.</li>
+          <li>Feminicídio: O assassinato de mulheres em razão do gênero é uma das formas mais extremas de violência.</li>
+          <li>Assédio sexual: O assédio em espaços públicos e privados é uma realidade que afeta muitas mulheres.</li>
+      </ul>
 
+      <h3>Desigualdade de gênero no mercado de trabalho</h3>
+      <ul style={{ color: 'black' }}>
+          <li>Disparidade salarial: Mulheres frequentemente recebem salários inferiores aos dos homens para realizar o mesmo trabalho.</li>
+          <li>Sub-representação em cargos de liderança: A presença feminina em posições de poder e tomada de decisão ainda é limitada.</li>
+          <li>Assédio e discriminação: Mulheres podem enfrentar assédio moral e sexual no ambiente de trabalho, além de discriminação por gênero.</li>
+          <li>Conciliação entre vida profissional e pessoal: A dupla jornada de trabalho, que inclui responsabilidades domésticas e familiares, ainda é um desafio para muitas mulheres.</li>
+      </ul>
+
+      <h3>Desafios na saúde:</h3>
+      <ul style={{ color: 'black' }}>
+          <li>Saúde reprodutiva: O acesso a serviços de saúde reprodutiva, como planejamento familiar e aborto seguro, ainda é limitado em muitas regiões.</li>
+          <li>Saúde mental: Mulheres podem enfrentar desafios específicos relacionados à saúde mental, como depressão pós-parto e ansiedade.</li>
+          <li>Violência obstétrica: Mulheres podem sofrer violência física e psicológica durante o parto.</li>
+      </ul>
+    </div>
     </div>
   );
 }
